f2e5086986a81887e02bb25225f5e4b22d4d42ea
Mara Karagianni <mara@multiplace.org>
2021-10-15 22:20:41 +0300

    Add files for user-define-module

---
+++ user_defined_modules/which_pi.py
@@ -0,0 +1,5 @@
+from custom_pi import pi
+from math import pi
+
+print(pi)
+print('custom pi', pi)
---
+++ user_defined_modules/which_pi_answer.py
@@ -0,0 +1,5 @@
+from custom_pi import pi as cus_pi
+from math import pi
+
+print(pi)
+print('custom pi', cus_pi())
