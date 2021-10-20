15c9853d6c2340c268cb5b6e435bad802502888c
Mara Karagianni <mara@multiplace.org>
2021-10-20 22:00:13 +0300

    Fix os input in staged code

--- 010-045-platform-module/platform_info2_staged.py
+++ 010-045-platform-module/platform_info2_staged.py
@@ -12,5 +12,5 @@ def ask_user_info(spec):
 
 
 print("Depending on your input we can show relevant info")
-inp = input("Enter one of the words in [machine, architecture, python]: ")
+inp = input("Enter one of the words in [OS, architecture, python]: ")
 print(ask_user_info(inp))
