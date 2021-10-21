de76f6414c61471b8c08a44be71d4b4351c08382
Mara Karagianni <mara@multiplace.org>
2021-10-21 18:22:41 +0300

    Fix typos

--- 020-025-string-introduction-nature/strings.py
+++ 020-025-string-introduction-nature/strings.py
@@ -1,5 +1,3 @@
 my_string = 'Blue skies'
 
 # Iterate through the string here
-for char in my_string:
-    print(char, end='**')
--- 020-025-string-introduction-nature/strings_answer.py
+++ 020-025-string-introduction-nature/strings_answer.py
@@ -1,3 +1,5 @@
 my_string = 'Blue skies'
 
 # Iterate through the string here
+for char in my_string:
+    print(char*2, end='**')
