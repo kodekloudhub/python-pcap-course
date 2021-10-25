21945947e1ed101470202ebad4a2b7488be78847
Mara Karagianni <mara@multiplace.org>
2021-10-25 13:10:39 +0300

    Fix name for dir string methods

--- string_methods/string_fix.py
+++
@@ -1,6 +0,0 @@
-txt = ",$,,   chocolate   banana ice-cream..."
-
-# Add more options in the strip()
-txt.strip("$ ")
-
-# Clear any whitespaces left and print the outcome
--- string_methods/string_fix_answer.py
+++
@@ -1,8 +0,0 @@
-txt = ",$,,   chocolate   banana ice-cream..."
-
-# Add more options in the strip()
-txt = txt.strip(",$ .")
-
-# Clear any whitespaces left and print the outcome
-txt = " ".join(txt.split())
-print(txt)
